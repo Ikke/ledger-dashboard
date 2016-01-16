f09dab7a7a0bd7f89a81046ef7b4cf8489726b54
Kevin Daudt <me@ikke.info>
2016-01-16 19:48:49 +0100

    Update testrunner to allow for single run

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-echo "Press enter to run tests"
 
-while read
-do
-    nosetests $1 2>&1 | color_test_results
-done
+if [[ $1 == "-c" ]]
+then
+    shift
+    while [[ $? -eq 0 ]]
+    do
+        nosetests $@ 2>&1 | utils/color_test_results
+
+        echo "Press enter to run tests"
+
+        read
+    done
+else
+    nosetests $@ 2>&1 | utils/color_test_results
+fi
+
